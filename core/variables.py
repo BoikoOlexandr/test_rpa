c5fe79660d2ef0c2820346202cd93845698b9724
BoikoOlexandr <hnupsant@gmail.com>
2023-02-01 20:03:11 +0200

    feat(rpa): add logger & workItems

---
+++ core/variables.py
@@ -0,0 +1,24 @@
+from RPA.Robocorp.WorkItems import WorkItems
+
+from core.logger.Logger import Logger
+
+
+class Variables:
+
+
+    def __init__(self):
+        self.search_phrase = "Ukraine"
+        self.section = "any"
+        self.number_of_month = 2
+        self.logger = Logger().get_logger()
+
+    def Execute(self):
+        try:
+            library = WorkItems()
+            library.get_input_work_item()
+            variables = library.get_work_item_variables()
+            self.search_phrase = variables['variables']
+            self.section = variables['section']
+            self.number_of_month = variables['number_of_month']
+        except KeyError:
+            self.logger.error("set default values")
