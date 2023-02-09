cfbb6b9978f4ee97fe490e7689755a217dca3b63
BoikoOlexandr <hnupsant@gmail.com>
2023-02-09 18:07:31 +0200

    fix(rpa) output dir

--- core/variables.py
+++ core/variables.py
@@ -11,7 +11,7 @@ class Variables:
             library.get_input_work_item()
             variables = library.get_work_item_variables()
             self.search_phrase: str = variables['search_phrase']
-            self.sections: [str] = [section.strip().lower() for section in variables['search_phrase'].split(',')]
+            self.sections: [str] = [section.strip().lower() for section in variables['section'].split(',')]
             self.number_of_month: int = self.validate_number_of_month(variables['number_of_month'])
             log.info("Variables has been set from work items")
         except (KeyError, TypeError, AttributeError):
